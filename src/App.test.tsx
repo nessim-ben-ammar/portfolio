89320137e764c375e0b8bebc08566594bbb5e086
Nessim Ben Ammar <nessim.benammar@gmail.com>
2025-06-12 22:37:57 +0200

    test: wrap App in MemoryRouter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Portfolio heading', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const heading = screen.getByRole('heading', { name: /portfolio/i });
+  expect(heading).toBeInTheDocument();
 });
